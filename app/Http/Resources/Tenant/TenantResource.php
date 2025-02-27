35df309f294bb11d4a7e7b630ad829313043aab8
Motaz Hesham <motazhesham01@gmail.com>
2025-02-27 10:55:27 +0200

    edit profile api

--- app/Http/Resources/Tenant/TenantResource.php
+++ app/Http/Resources/Tenant/TenantResource.php
@@ -14,12 +14,17 @@ class TenantResource extends JsonResource
      */
     public function toArray($request)
     {
+        $client = $this->client;
+
         return [
             'id' => $this->id, 
-            'name' => $this->name,
-            'phone' => $this->phone,
-            'email' => $this->email,
-            'address' => $this->client->address ?? '',
+            'name' => $this->name ?? '',
+            'phone' => $this->phone ?? '',
+            'email' => $this->email ?? '',
+            'contract' => optional($client?->clientContracts()?->latest('created_at')->first())->only([
+                'id', 'start_date', 'end_date', 'status'
+            ]),
+            'address' => $client?->address ?? '',
         ];
     }
 }
